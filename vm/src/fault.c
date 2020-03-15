0acf791694b79ca1aac6b9f84acd3e33b36af9a0
angelsl <angelsl@in04.sg>
2020-03-16 03:06:30 +0800

    Abort when sifault is called in debug mode

--- vm/src/fault.c
+++ vm/src/fault.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 #include "fault.h"
 #include "vm.h"
 
@@ -5,5 +7,6 @@ jmp_buf sinter_fault_jmp = { 0 };
 
 _Noreturn void sifault(enum sinter_fault reason) {
   sistate.fault_reason = reason;
+  assert("Faulting." == 0);
   longjmp(sinter_fault_jmp, 1);
 }
