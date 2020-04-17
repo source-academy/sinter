8de4a3bff7da6cf6bb403c938cdf49fc6ef5edb6
angelsl <angelsl@in04.sg>
2020-04-18 02:06:44 +0800

    Swap test diff direction and use unified diff

--- vm_test/run_test.sh
+++ vm_test/run_test.sh
@@ -4,4 +4,4 @@ runner="$1"
 in_file="$2.svm"
 out_file="$2.out"
 
-"$runner" "$in_file" | diff - "$out_file"
+"$runner" "$in_file" | diff -u "$out_file" -
--- vm_test/run_test_stderr.sh
+++ vm_test/run_test_stderr.sh
@@ -4,4 +4,4 @@ runner="$1"
 in_file="$2.svm"
 out_file="$2.out"
 
-"$runner" "$in_file" 2>&1 | sed -e 's/[0-9A-F]\{7,\}/MEMADDR/ig' | diff - "$out_file"
+"$runner" "$in_file" 2>&1 | sed -e 's/[0-9A-F]\{7,\}/MEMADDR/ig' | diff -u "$out_file" -
