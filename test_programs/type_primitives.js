a1bea5667dc551756b810295a0a361608091c954
angelsl <angelsl@in04.sg>
2020-04-26 21:14:45 +0800

    Add tests for is_* functions

---
+++ test_programs/type_primitives.js
@@ -0,0 +1,81 @@
+let a = 1;
+let b = [];
+let c = () => 1;
+let d = "a";
+let e = undefined;
+let f = null;
+let g = true;
+let h = false;
+let i = display;
+
+display(is_array    (a));
+display(is_boolean  (a));
+display(is_function (a));
+display(is_null     (a));
+display(is_number   (a));
+display(is_string   (a));
+display(is_undefined(a));
+
+display(is_array    (b));
+display(is_boolean  (b));
+display(is_function (b));
+display(is_null     (b));
+display(is_number   (b));
+display(is_string   (b));
+display(is_undefined(b));
+
+display(is_array    (c));
+display(is_boolean  (c));
+display(is_function (c));
+display(is_null     (c));
+display(is_number   (c));
+display(is_string   (c));
+display(is_undefined(c));
+
+display(is_array    (d));
+display(is_boolean  (d));
+display(is_function (d));
+display(is_null     (d));
+display(is_number   (d));
+display(is_string   (d));
+display(is_undefined(d));
+
+display(is_array    (e));
+display(is_boolean  (e));
+display(is_function (e));
+display(is_null     (e));
+display(is_number   (e));
+display(is_string   (e));
+display(is_undefined(e));
+
+display(is_array    (f));
+display(is_boolean  (f));
+display(is_function (f));
+display(is_null     (f));
+display(is_number   (f));
+display(is_string   (f));
+display(is_undefined(f));
+
+display(is_array    (g));
+display(is_boolean  (g));
+display(is_function (g));
+display(is_null     (g));
+display(is_number   (g));
+display(is_string   (g));
+display(is_undefined(g));
+
+display(is_array    (h));
+display(is_boolean  (h));
+display(is_function (h));
+display(is_null     (h));
+display(is_number   (h));
+display(is_string   (h));
+display(is_undefined(h));
+
+display(is_array    (i));
+display(is_boolean  (i));
+display(is_function (i));
+display(is_null     (i));
+display(is_number   (i));
+display(is_string   (i));
+display(is_undefined(i));
