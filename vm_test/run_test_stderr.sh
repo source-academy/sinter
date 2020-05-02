d11d9b7a697be30210a1613281ba1cec92a01b90
angelsl <angelsl@in04.sg>
2020-05-02 16:43:14 +0800

    Replace even shorter memory addresses

--- vm_test/run_test_stderr.sh
+++ vm_test/run_test_stderr.sh
@@ -6,4 +6,4 @@ runner="$1"
 in_file="$2.svm"
 out_file="$2.out"
 
-"$runner" "$in_file" 2>&1 | sed -e 's/[0-9A-F]\{7,\}/MEMADDR/ig' | diff -u "$out_file" -
+"$runner" "$in_file" 2>&1 | sed -e 's/0x[0-9A-F]\{5,\}/0xMEMADDR/ig' | diff -u "$out_file" -
